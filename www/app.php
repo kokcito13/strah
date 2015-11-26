eb1666bd31d919e7a83e6d0f348e8dd9e6efb825
kokcito13 <oklosovich@gmail.com>
2015-11-26 18:06:55 +0200

    make 301 redirect

--- www/app.php
+++ www/app.php
@@ -1,4 +1,34 @@
 <?php
+//redirect 301
+$urlCurrentPath = $_SERVER['REQUEST_URI'];
+$urlPath = array(
+    '/moscow/companies/rostov-na-donu/gefest' => "http://dvestrahovki.ru/rostov-na-donu/companies/gefest",
+    '/krasnodar/gefest' => "http://dvestrahovki.ru/krasnodar/companies/gefest",
+    '/moscow/companies/rostov-na-donu/gefest/comments' => "http://dvestrahovki.ru/rostov-na-donu/companies/gefest/comments",
+    '/moscow/companies/novosibirsk/nadins' => "http://dvestrahovki.ru/novosibirsk/companies/nadins",
+    '/company/ru/moscow/evro-polis' => "http://dvestrahovki.ru/moscow/companies/evro-polis",
+    '/vtb' => "http://dvestrahovki.ru/moscow/companies/vtb",
+    '/company/ru/moscow/ugsk' => "http://dvestrahovki.ru/moscow/companies/ugsk",
+    '/company/ru/nizhny-novgorod/ugsk' => "http://dvestrahovki.ru/nizhny-novgorod/companies/ugsk",
+    '/company/ru/rostov-na-donu/zettains' => "http://dvestrahovki.ru/rostov-na-donu/companies/zettains",
+    '/company/ru/sankt-peterburg/nig' => "http://dvestrahovki.ru/sankt-peterburg/companies/nig",
+    '/company/ru/moscow/vtb' => "http://dvestrahovki.ru/moscow/companies/vtb",
+    '/company/ru/krasnodar/gefest' => "http://dvestrahovki.ru/krasnodar/companies/gefest",
+    '/company/ru/krasnodar/zhaso' => "http://dvestrahovki.ru/krasnodar/companies/zhaso",
+    '/novosibirsk/companies/alfastrah/comments' => "http://dvestrahovki.ru/krasnodar/companies/alfastrah/comments",
+    '/moscow/companies/novosibirsk/alfastrah' => "http://dvestrahovki.ru/krasnodar/companies/alfastrah",
+    '/moscow/companies/novosibirsk/nadins' => "http://dvestrahovki.ru/novosibirsk/companies/nadins",
+    '/sankt-peterburg/liberty' => "http://dvestrahovki.ru/sankt-peterburg/companies/liberty",
+    '/moscow/companies/rostov-na-donu' => "http://dvestrahovki.ru/rostov-na-donu/companies",
+    '/moscow/companies/Samara/alfastrah/comments' => "http://dvestrahovki.ru/samara/companies/alfastrah/comments",
+    '/moscow/companies/Novosibirsk/allianz/comments' => "http://dvestrahovki.ru/novosibirsk/companies/allianz/comments",
+);
+if (isset($urlPath[$urlCurrentPath])) {
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: ".$urlPath[$urlCurrentPath]);
+    exit();
+}
+// end redirect 301
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
